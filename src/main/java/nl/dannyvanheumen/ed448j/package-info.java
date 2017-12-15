30faea348d607afab64afe6a135c37e21a90a949
Danny van Heumen <danny@dannyvanheumen.nl>
2017-12-15 17:34:52 +0100

    Set-up package parameters.

---
+++ src/main/java/nl/dannyvanheumen/ed448j/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * An implementation of Ed448-Goldilocks elliptic curve for Java.
+ */
+@ParametersAreNonnullByDefault
+package nl.dannyvanheumen.ed448j;
+
+import javax.annotation.ParametersAreNonnullByDefault;
