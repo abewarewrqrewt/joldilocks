66699a55b60152b58aca97aed420c7227cabce26
Danny van Heumen <danny@dannyvanheumen.nl>
2017-12-17 00:17:28 +0100

    Renamed package.

--- src/main/java/nl/dannyvanheumen/joldilocks/package-info.java
+++ src/main/java/nl/dannyvanheumen/joldilocks/package-info.java
@@ -2,6 +2,6 @@
  * An implementation of Ed448-Goldilocks elliptic curve for Java.
  */
 @ParametersAreNonnullByDefault
-package nl.dannyvanheumen.ed448j;
+package nl.dannyvanheumen.joldilocks;
 
 import javax.annotation.ParametersAreNonnullByDefault;
