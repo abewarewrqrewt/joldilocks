e1b4dd368ca82c7489f1dcd0c9e6746ac685a49c
Danny van Heumen <danny@dannyvanheumen.nl>
2018-01-20 00:24:12 +0100

    Updated package description.

--- src/main/java/nl/dannyvanheumen/joldilocks/package-info.java
+++ src/main/java/nl/dannyvanheumen/joldilocks/package-info.java
@@ -1,5 +1,8 @@
 /**
  * An implementation of Ed448-Goldilocks elliptic curve for Java.
+ * <p>
+ * This implementation is geared towards use in an OTR version 4 implementation. Therefore it might not be complete and
+ * it does not provide every function defined in Ed448-Goldilocks.
  */
 @ParametersAreNonnullByDefault
 package nl.dannyvanheumen.joldilocks;
