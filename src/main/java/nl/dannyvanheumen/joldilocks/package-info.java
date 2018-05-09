f38b12795ba5e724935ff5566bb58f58c8887eef
Danny van Heumen <danny@dannyvanheumen.nl>
2018-05-09 21:19:28 +0200

    Comments on public interface.

--- src/main/java/nl/dannyvanheumen/joldilocks/package-info.java
+++ src/main/java/nl/dannyvanheumen/joldilocks/package-info.java
@@ -3,6 +3,11 @@
  * <p>
  * This implementation is geared towards use in an OTR version 4 implementation. Therefore it might not be complete and
  * it does not provide every function defined in Ed448-Goldilocks.
+ *
+ * Public interfaces: (see package-private vs public classes, utilities)
+ *  - Point
+ *  - Ed448
+ *  - KeyPair
  */
 // TODO Modify the code such that public keys are persisted as byte arrays. (Is this necessary for security, otherwise in-memory objects are more convenient.)
 // TODO Consider temporary instances creating during calculations that may contain sensitive data. Can we reduce allocations/securely clear this data?
