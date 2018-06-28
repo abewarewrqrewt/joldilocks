9bf6a16b10451eacc1556d70a4e0cd23d1c84843
Danny van Heumen <danny@dannyvanheumen.nl>
2018-06-29 00:23:18 +0200

    Add assertion for known issue with signing code.

--- src/main/java/nl/dannyvanheumen/joldilocks/Ed448.java
+++ src/main/java/nl/dannyvanheumen/joldilocks/Ed448.java
@@ -233,8 +233,9 @@ public final class Ed448 {
         // "6. Form the signature of the concatenation of R (57 octets) and the little-endian encoding of S (57 octets;
         //    the ten most significant bits of the final octets are always zero)."
         // Given that the top ten most significant bits are always zero, add single byte to get to total of 114 bytes.
-        // TODO: Should we always add one zero byte, or is there a possibility that we need to add more on occasion?
+        // FIXME This is an actual issue! Should we always add one zero byte, or is there a possibility that we need to add more on occasion?
         final byte[] signature = concatenate(encodedPointR, encodedPointS, new byte[1]);
+        assert signature.length == SIGNATURE_LENGTH_BYTES : "Generated signature is of unexpected size. This is a bug in the current code.";
         // "7. Securely delete 'sym_key', 'sk', 'h', 'r' and 'k'."
         clear(sbytes);
         clear(h);
